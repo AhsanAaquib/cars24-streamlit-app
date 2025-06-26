148dc770a70299d8c770b02f02a71a29f66bebd3
AhsanAaquib <ahsanua1994@gmail.com>
2025-06-26 11:42:04 +0530

    Fix path to model and scaler files for Streamlit

--- ML_OPS_Day_2/cars_24_streamlit.py
+++ ML_OPS_Day_2/cars_24_streamlit.py
@@ -11,8 +11,10 @@ encode_dict = {
     'owner': {'First Owner': 1, 'Second Owner': 2, 'Third Owner': 3, 'Fourth & Above Owner': 4, 'Test Drive Car': 5}    
 }
 
+model = joblib.load('ML_OPS_Day_2/car_price_model.pkl')
+
 import joblib
-model = joblib.load('car_price_model.pkl')
+# model = joblib.load('car_price_model.pkl')
 
 # Eneter the details of the car
 
@@ -38,7 +40,7 @@ max_power = st.number_input("Max Power (bhp)", min_value=0.0, max_value=300.0, v
 
 seats = st.number_input("Number of Seats", min_value=2, max_value=10, value=5, step=1)  
 
-scaler = joblib.load('scaler.pkl')
+scaler = joblib.load('ML_OPS_Day_2/scaler.pkl')
 
 def model_predict(year, km_driven, fuel, seller_type, transmission, owner, mileage, engine, max_power, seats):
     # Encode categorical variables
